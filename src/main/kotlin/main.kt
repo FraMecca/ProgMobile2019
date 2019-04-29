6bc6f9cda3dc2f2334d98e8eb8a7789e9f85df42
Francesco Mecca <me@francescomecca.eu>
2019-04-29 09:30:01 +0200

    fst

---
+++ src/main/kotlin/main.kt
@@ -0,0 +1,68 @@
+package com.streaming.main
+
+import io.vertx.core.*
+import io.vertx.core.Handler
+import io.vertx.core.buffer.Buffer
+import io.vertx.core.http.ServerWebSocket
+import io.vertx.core.shareddata.LocalMap
+import io.vertx.core.shareddata.SharedData
+import java.util.concurrent.atomic.AtomicInteger
+
+
+var i = AtomicInteger(0)
+
+fun logic(vertx: Vertx ,ws: ServerWebSocket){
+
+    var action = "idle"
+
+    ws.closeHandler({ ch ->
+        println("Closing ws-connection to client " + ws.textHandlerID())
+        println("Closed ws-con")
+    })
+
+    ws.handler(object:Handler<Buffer> {
+        override fun handle(data:Buffer) {
+            val cnt = i.getAndAdd(1)
+            action = "send"
+
+            val msg = data.getString(0, data.length())
+            println("Message from ws-client " + ws.textHandlerID()+ ": " + msg)
+            val resp = Buffer.buffer()
+            resp.appendString("O Ding Dong asd asda sdasd a")
+
+            var size = 0
+            while(action == "send") {
+                ws.write(resp)
+                size++
+                if(size >= 10)
+                    action = "idle"
+            }
+
+        }
+    })
+}
+
+fun main(args: Array<String>){
+    val vertx = Vertx.vertx()
+    val server = vertx.createHttpServer()
+
+    server.websocketHandler({ ws->
+        println("Websocket-handshake...")
+        println("path = " + ws.path())
+        println("uri = " + ws.uri())
+        println("localAdress = " + ws.localAddress().toString())
+        println("remoteAddress = " + ws.remoteAddress())
+        println(ws.toString())
+
+        if (!ws.path().equals("/chat")){
+            ws.reject()
+        }else{
+            logic(vertx, ws)
+        } })
+
+    server.listen(8080, { res-> if (res.succeeded()) {
+        println("Listening...")
+    }else{
+        println(("Failed to bind!"))
+    } })
+}
