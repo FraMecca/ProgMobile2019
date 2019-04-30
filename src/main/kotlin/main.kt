1d25f4264818b497d9fd0a6f3fc5f0cf51433ea9
Francesco Mecca <me@francescomecca.eu>
2019-04-30 12:36:35 +0200

    runCommand

--- src/main/kotlin/main.kt
+++ src/main/kotlin/main.kt
@@ -7,7 +7,24 @@ import io.vertx.core.http.ServerWebSocket
 import io.vertx.core.shareddata.LocalMap
 import io.vertx.core.shareddata.SharedData
 import java.util.concurrent.atomic.AtomicInteger
+import java.io.File
+import java.io.IOException
+import java.io.InputStream
 
+fun String.runCommand(workingDir: File): InputStream? {
+    try {
+        val parts = this.split("\\s".toRegex())
+        val proc = ProcessBuilder(*parts.toTypedArray())
+            .directory(workingDir)
+            .redirectOutput(ProcessBuilder.Redirect.PIPE)
+            .redirectError(ProcessBuilder.Redirect.PIPE)
+            .start()
+        return proc.inputStream
+    } catch(e: IOException) {
+        e.printStackTrace()
+        return null
+    }
+}
 
 var i = AtomicInteger(0)
 
@@ -28,10 +45,17 @@ fun logic(vertx: Vertx ,ws: ServerWebSocket){
             val msg = data.getString(0, data.length())
             println("Message from ws-client " + ws.textHandlerID()+ ": " + msg)
             val resp = Buffer.buffer()
-            resp.appendString("O Ding Dong asd asda sdasd a")
+           // resp.appendString("O Ding Dong asd asda sdasd a")
 
             var size = 0
+            val file = "/home/user/out.webm"
+            val command = "ffmpeg -i " + file + " -f ogg -q 5 pipe:1"
+            val oggStream = command.runCommand(File("/home/user"))
             while(action == "send") {
+                val buf = oggStream?.readBytes()
+                resp.setBytes(0, buf)
+                print(resp)
+                println(" " + buf)
                 ws.write(resp)
                 size++
                 if(size >= 10)
